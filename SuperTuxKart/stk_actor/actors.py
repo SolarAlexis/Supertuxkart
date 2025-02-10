40395163264356a5822cee3b684416e357046f16
Solar Alexis <asolar.athle@gmail.com>
2025-02-10 22:02:11 +0100

    update SquashedDiagGaussianDistribution

--- SuperTuxKart/stk_actor/actors.py
+++ SuperTuxKart/stk_actor/actors.py
@@ -23,7 +23,8 @@ from torch.distributions import (
     TransformedDistribution,
     TanhTransform,
 )
-from bbrl.utils.distributions import SquashedDiagGaussianDistribution
+from abc import ABC, abstractmethod
+from typing import Optional, Tuple, Union, List
 
 class FeatureFilterWrapper(gym.ObservationWrapper):
     def __init__(self, env, index):
@@ -361,6 +362,306 @@ class DiscretePolicy(Agent):
             self.set((f"{self.prefix}entropy", t), entropy)
 
 # TQC part ---------------------------------------------------------------
+
+class Distribution(ABC):
+    """Abstract base class for distributions."""
+
+    def __init__(self):
+        super().__init__()
+        self.distribution = None
+
+    @abstractmethod
+    def proba_distribution_net(
+        self, *args, **kwargs
+    ) -> Union[nn.Module, Tuple[nn.Module, nn.Parameter]]:
+        """Create the layers and parameters that represent the distribution.
+
+        Subclasses must define this, but the arguments and return type vary between
+        concrete classes."""
+
+    @abstractmethod
+    def proba_distribution(self, *args, **kwargs) -> "Distribution":
+        """Set parameters of the distribution.
+
+        :return: self
+        """
+
+    @abstractmethod
+    def log_prob(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Returns the log likelihood
+
+        :param x: the taken action
+        :return: The log likelihood of the distribution
+        """
+
+    @abstractmethod
+    def entropy(self) -> Optional[torch.Tensor]:
+        """
+        Returns Shannon's entropy of the probability
+
+        :return: the entropy, or None if no analytical form is known
+        """
+
+    @abstractmethod
+    def sample(self) -> torch.Tensor:
+        """
+        Returns a sample from the probability distribution
+
+        :return: the stochastic action
+        """
+
+    @abstractmethod
+    def mode(self) -> torch.Tensor:
+        """
+        Returns the most likely action (deterministic output)
+        from the probability distribution
+
+        :return: the stochastic action
+        """
+
+    def get_actions(self, deterministic: bool = False) -> torch.Tensor:
+        """
+        Return actions according to the probability distribution.
+
+        :param deterministic:
+        :return:
+        """
+        if deterministic:
+            return self.mode()
+        return self.sample()
+
+    @abstractmethod
+    def actions_from_params(self, *args, **kwargs) -> torch.Tensor:
+        """
+        Returns samples from the probability distribution
+        given its parameters.
+
+        :return: actions
+        """
+
+    @abstractmethod
+    def log_prob_from_params(self, *args, **kwargs) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Returns samples and the associated log probabilities
+        from the probability distribution given its parameters.
+
+        :return: actions and log prob
+        """
+
+
+def sum_independent_dims(tensor: torch.Tensor) -> torch.Tensor:
+    """
+    Continuous actions are usually considered to be independent,
+    so we can sum components of the ``log_prob`` or the entropy.
+
+    :param tensor: shape: (n_batch, n_actions) or (n_batch,)
+    :return: shape: (n_batch,)
+    """
+    if len(tensor.shape) > 1:
+        tensor = tensor.sum(dim=1)
+    else:
+        tensor = tensor.sum()
+    return tensor
+
+class DiagGaussianDistribution(Distribution):
+    """
+    Gaussian distribution with diagonal covariance matrix, for continuous actions.
+
+    :param action_dim:  Dimension of the action space.
+    """
+
+    def __init__(self, action_dim: int):
+        super().__init__()
+        self.action_dim = action_dim
+        self.mean_actions = None
+        self.log_std = None
+
+    def proba_distribution_net(
+        self, latent_dim: int, log_std_init: float = 0.0
+    ) -> Tuple[nn.Module, nn.Parameter]:
+        """
+        Create the layers and parameter that represent the distribution:
+        one output will be the mean of the Gaussian, the other parameter will be the
+        standard deviation (log std in fact to allow negative values)
+
+        :param latent_dim: Dimension of the last layer of the policy (before the action layer)
+        :param log_std_init: Initial value for the log standard deviation
+        :return:
+        """
+        mean_actions = nn.Linear(latent_dim, self.action_dim)
+        # TODO: allow action dependent std
+        log_std = nn.Parameter(
+            torch.ones(self.action_dim) * log_std_init, requires_grad=True
+        )
+        return mean_actions, log_std
+
+    def make_distribution(
+        self, mean_actions: torch.Tensor, log_std: torch.Tensor
+    ) -> "DiagGaussianDistribution":
+        """
+        Create the distribution given its parameters (mean, std)
+
+        :param mean_actions:
+        :param log_std:
+        :return:
+        """
+        action_std = torch.ones_like(mean_actions) * log_std.exp()
+        self.distribution = Normal(mean_actions, action_std)
+        return self
+
+    def log_prob(self, actions: torch.Tensor) -> torch.Tensor:
+        """
+        Get the log probabilities of actions according to the distribution.
+        Note that you must first call the ``proba_distribution()`` method.
+
+        :param actions:
+        :return:
+        """
+        log_prob = self.distribution.log_prob(actions)
+        return sum_independent_dims(log_prob)
+
+    def entropy(self) -> torch.Tensor:
+        return sum_independent_dims(self.distribution.entropy())
+
+    def sample(self) -> torch.Tensor:
+        # Reparametrization trick to pass gradients
+        return self.distribution.rsample()
+
+    def mode(self) -> torch.Tensor:
+        return self.distribution.mean
+
+    def actions_from_params(
+        self, mean_actions: torch.Tensor, log_std: torch.Tensor, deterministic: bool = False
+    ) -> torch.Tensor:
+        # Update the proba distribution
+        self.proba_distribution(mean_actions, log_std)
+        return self.get_actions(deterministic=deterministic)
+
+    def log_prob_from_params(
+        self, mean_actions: torch.Tensor, log_std: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Compute the log probability of taking an action
+        given the distribution parameters.
+
+        :param mean_actions:
+        :param log_std:
+        :return:
+        """
+        actions = self.actions_from_params(mean_actions, log_std)
+        log_prob = self.log_prob(actions)
+        return actions, log_prob
+
+class TanhBijector:
+    """
+    Bijective transformation of a probability distribution
+    using a squashing function (tanh)
+    TODO: use Pyro instead (https://pyro.ai/)
+
+    :param epsilon: small value to avoid NaN due to numerical imprecision.
+    """
+
+    def __init__(self, epsilon: float = 1e-6):
+        super().__init__()
+        self.epsilon = epsilon
+
+    @staticmethod
+    def forward(x: torch.Tensor) -> torch.Tensor:
+        return torch.tanh(x)
+
+    @staticmethod
+    def atanh(x: torch.Tensor) -> torch.Tensor:
+        """
+        Inverse of Tanh
+
+        Taken from Pyro: https://github.com/pyro-ppl/pyro
+        0.5 * torch.log((1 + x ) / (1 - x))
+        """
+        return 0.5 * (x.log1p() - (-x).log1p())
+
+    @staticmethod
+    def inverse(y: torch.Tensor) -> torch.Tensor:
+        """
+        Inverse tanh.
+
+        :param y:
+        :return:
+        """
+        eps = torch.finfo(y.dtype).eps
+        # Clip the action to avoid NaN
+        return TanhBijector.atanh(y.clamp(min=-1.0 + eps, max=1.0 - eps))
+
+    def log_prob_correction(self, x: torch.Tensor) -> torch.Tensor:
+        # Squash correction (from original SAC implementation)
+        return torch.log(1.0 - torch.tanh(x) ** 2 + self.epsilon)
+
+class SquashedDiagGaussianDistribution(DiagGaussianDistribution):
+    """
+    Gaussian distribution with diagonal covariance matrix, followed by a squashing function (tanh) to ensure bounds.
+
+    :param action_dim: Dimension of the action space.
+    :param epsilon: small value to avoid NaN due to numerical imprecision.
+    """
+
+    def __init__(self, action_dim: int, epsilon: float = 1e-6):
+        super().__init__(action_dim)
+        # Avoid NaN (prevents division by zero or log of zero)
+        self.epsilon = epsilon
+        self.gaussian_actions = None
+
+    def proba_distribution(
+        self, mean_actions: torch.Tensor, log_std: torch.Tensor
+    ) -> "SquashedDiagGaussianDistribution":
+        super().proba_distribution(mean_actions, log_std)
+        return self
+
+    def log_prob(
+        self, actions: torch.Tensor, gaussian_actions: Optional[torch.Tensor] = None
+    ) -> torch.Tensor:
+        # Inverse tanh
+        # Naive implementation (not stable): 0.5 * torch.log((1 + x) / (1 - x))
+        # We use numpy to avoid numerical instability
+        if gaussian_actions is None:
+            # It will be clipped to avoid NaN when inversing tanh
+            gaussian_actions = TanhBijector.inverse(actions)
+
+        # Log likelihood for a Gaussian distribution
+        log_prob = super().log_prob(gaussian_actions)
+        # Squash correction (from original SAC implementation)
+        # this comes from the fact that tanh is bijective and differentiable
+        log_prob -= torch.sum(torch.log(1 - actions**2 + self.epsilon), dim=1)
+        return log_prob
+
+    def entropy(self) -> Optional[torch.Tensor]:
+        # No analytical form,
+        # entropy needs to be estimated using -log_prob.mean()
+        raise Exception("Call to entropy in squashed Diag Gaussian distribution")
+        return None
+
+    def sample(self) -> torch.Tensor:
+        # Reparametrization trick to pass gradients
+        self.gaussian_actions = super().sample()
+        return torch.tanh(self.gaussian_actions)
+
+    def mode(self) -> torch.Tensor:
+        self.gaussian_actions = super().mode()
+        # Squash the output
+        return torch.tanh(self.gaussian_actions)
+
+    def log_prob_from_params(
+        self, mean_actions: torch.Tensor, log_std: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        action = self.actions_from_params(mean_actions, log_std)
+        log_prob = self.log_prob(action, self.gaussian_actions)
+        return action, log_prob
+
+    def get_ten_samples(self) -> List:
+        action_list = []
+        for i in range(10):
+            action = self.sample()
+            action_list.append(action)
+        return action_list
 
 class BaseActor(Agent):
     """ Generic class to centralize copy_parameters"""
